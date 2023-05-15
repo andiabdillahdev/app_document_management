2498f00f326ac0278e5ec4865fafe8fe40615ed1
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-16 06:05:15 +0800

    delete build folder

--- build/controller/dashboardController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dashboard = (req, res) => {
-    var _a;
-    const user = (_a = req.session) === null || _a === void 0 ? void 0 : _a.user;
-    res.render("pages/dashboard/index", { user });
-};
-exports.default = { dashboard };
